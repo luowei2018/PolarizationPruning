c8be1c5f9a2f0bf2c26f656d9f2b8d524cdc1c6b
Bo Chen <bochen1993cs@gmail.com>
2022-07-05 16:09:57 -0700

    l

--- cifar/models/common.py
+++ cifar/models/common.py
@@ -180,7 +180,7 @@ def prune_conv_layer(conv_layer: Union[nn.Conv2d, nn.Linear],
         Note: `in_channel_mask` is CONFLICT with `sparse_layer_in`!
     :return out_channel_mask
     """
-    fake_prune = True
+    fake_prune = False
     assert isinstance(conv_layer, nn.Conv2d) or isinstance(conv_layer, nn.Linear), f"conv_layer got {conv_layer}"
 
     assert isinstance(sparse_layer, nn.BatchNorm2d) or \
