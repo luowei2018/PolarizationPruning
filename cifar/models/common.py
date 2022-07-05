02c8e2775bb0e2991d27a87291558117ed1ae5b8
Bo Chen <bochen1993cs@gmail.com>
2022-07-05 16:40:28 -0700

    Update common.py

--- cifar/models/common.py
+++ cifar/models/common.py
@@ -301,8 +301,8 @@ def prune_conv_layer(conv_layer: Union[nn.Conv2d, nn.Linear],
         # prune the bn layer
         if bn_layer is not None:
             if fake_prune:
-                bn_layer.weight.data[idx_block.tolist()] *= 0.0
-                bn_layer.bias.data[idx_block.tolist()] *= 0.0
+                bn_layer.weight.data[idx_block.tolist()] = 0
+                bn_layer.bias.data[idx_block.tolist()] = 0
                 pass
             else:
                 bn_layer.weight.data = bn_layer.weight.data[idx_out.tolist()].clone()
