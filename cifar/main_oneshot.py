4e2c2e81b03d03721333fe316692201544f54ac9
Bo Chen <bochen1993cs@gmail.com>
2022-07-07 11:53:04 -0700

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -565,6 +565,7 @@ def log_quantization(model):
     remain = torch.ones(total_channels).long().cuda()
     assigned_binindices = torch.zeros(total_channels).long().cuda()
     assigned_binindices[:] = -1
+    print(bin_indices)
     
     for bin_idx in bin_indices[:-1]:
         dist = torch.abs(torch.log10(args.bins[bin_idx]/all_scale_factors)) 
