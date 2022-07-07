bfdda2ecbff0b1d53d00dcf773bf524c68596c46
Bo Chen <bochen1993cs@gmail.com>
2022-07-07 07:33:38 -0700

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -346,7 +346,7 @@ if args.resume:
         args.start_epoch = checkpoint['epoch']
         best_prec1 = checkpoint['best_prec1']
         model.load_state_dict(checkpoint['state_dict'])
-        #optimizer.load_state_dict(checkpoint['optimizer'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
 
         print("=> loaded checkpoint '{}' (epoch {}) Prec1: {:f}"
               .format(args.resume, checkpoint['epoch'], best_prec1))
