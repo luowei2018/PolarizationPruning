a441b219b3ca04abf31d9472bfb90dd0793c7f0c
Bo Chen <bochen1993cs@gmail.com>
2022-07-11 16:00:59 -0700

    Update main_oneshot.py

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -1084,8 +1084,10 @@ def log_quantization(model, args):
         
     bn_modules = []
     for name, m in model.named_modules():
+        print(name)
         if isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
             bn_modules.append(m)
+    exit(0)
     
     all_scale_factors = torch.tensor([]).cuda()
     for bn_module in bn_modules:
@@ -1379,7 +1381,7 @@ def train(train_loader, model, criterion, optimizer, epoch, sparsity, args, is_d
             loss += sparsity_loss
             avg_sparsity_loss.update(sparsity_loss.data.item(), image.size(0))
         
-        check_model_np_nan(model,'1')
+        #check_model_np_nan(model,'1')
         loss.backward()
         if args.loss == LossType.L1_SPARSITY_REGULARIZATION:
             updateBN(model, sparsity,
@@ -1390,9 +1392,9 @@ def train(train_loader, model, criterion, optimizer, epoch, sparsity, args, is_d
         # BN_grad_zero(model)
         if args.loss in {LossType.LOG_QUANTIZATION}:
             log_quantization(model, args)
-        check_model_np_nan(model,'2')
+        #check_model_np_nan(model,'2')
         optimizer.step()
-        check_model_np_nan(model,'3')
+        #check_model_np_nan(model,'3')
         if args.loss in {LossType.POLARIZATION,
                          LossType.POLARIZATION_GRAD,
                          LossType.L2_POLARIZATION} or \
