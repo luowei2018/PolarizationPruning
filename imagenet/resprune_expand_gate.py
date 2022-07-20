81e6d8b85408a33c3ae619354336b231a4a71746
Bo Chen <bochen1993cs@gmail.com>
2022-07-20 11:27:33 -0700

    Update resprune_expand_gate.py

--- imagenet/resprune_expand_gate.py
+++ imagenet/resprune_expand_gate.py
@@ -124,7 +124,7 @@ def prune_resnet(sparse_model: torch.nn.Module, pruning_strategy: str, sanity_ch
 
     pruned_model.prune_model(pruner=pruner,
                              prune_mode=prune_mode)
-    return prune_model
+    return pruned_model
     #print("Pruning finished. cfg:")
     #print(pruned_model.config())
 
