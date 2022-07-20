cfc2ece48180b2e8f6d8eb595280406440d8c877
Bo Chen <bochen1993cs@gmail.com>
2022-07-20 16:40:44 -0700

    Update main_oneshot.py

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -626,8 +626,8 @@ def main_worker(gpu, ngpus_per_node, args):
     print("rank #{}: dataloader loaded!".format(args.rank))
     
     if args.evaluate:
-        prec1 = validate(val_loader, model, criterion, epoch=0, args=args, writer=None)
-        factor_visualization(0, model, args, prec1)
+        #prec1 = validate(val_loader, model, criterion, epoch=0, args=args, writer=None)
+        factor_visualization(0, model, args, 0)
         prune_while_training(model, args.arch, args.prune_mode, args.width_multiplier, val_loader, criterion, 0, args)
         return
 
