df3db513f4c91b28e595f3c48a5c68891aba232f
Bo Chen <bochen1993cs@gmail.com>
2022-08-04 14:46:14 -0700

    Update common.py

--- imagenet/models/common.py
+++ imagenet/models/common.py
@@ -252,8 +252,10 @@ def prune_conv_layer(conv_layer: nn.Conv2d,
         if fake_prune:
             if idx_block.tolist():
                 print(sparse_layer_out.bias.data[idx_block.tolist()].abs().max())
+            print(sparse_layer_out.bias.data)
             sparse_layer_out.weight.data[idx_block.tolist()] = 0
-            sparse_layer_out.bias.data[idx_block.tolist()] = 1
+            sparse_layer_out.bias.data[idx_block.tolist()] = 0
+            print(sparse_layer_out.bias.data)
         else:
             sparse_layer_out.weight.data = sparse_layer_out.weight.data[idx_out.tolist()].clone()
             sparse_layer_out.bias.data = sparse_layer_out.bias.data[idx_out.tolist()].clone()
