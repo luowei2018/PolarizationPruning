6401ee1e1c935cd15de44f41a8bf7e6c9bdbdf16
Bo Chen <bochen1993cs@gmail.com>
2022-08-10 11:06:20 -0700

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -495,8 +495,8 @@ def bn_sparsity(model, loss_type, sparsity, t, alpha,
         raise ValueError()
      
 if args.bin_mode ==2:
-    #args.bins = torch.pow(10.,torch.tensor([-10,-4,-2,-.5])).cuda(0)
-    args.bins = torch.tensor([1e-10,1e-4,1e-2,1e.5]).cuda(0)
+    args.bins = torch.pow(10.,torch.tensor([-10,-4,-2,.5])).cuda(0)
+    #args.bins = torch.tensor([1e-10,1e-4,1e-2,1e0.5]).cuda(0)
 elif args.bin_mode == 1:
     args.bins = torch.pow(10.,torch.tensor([-5,-4,-3,-2,-1,0])).cuda(0)
 else:
