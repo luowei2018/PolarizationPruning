c111ed87acb097224b781e5ecfaf9786440be40f
Bo Chen <bochen1993cs@gmail.com>
2022-08-14 10:58:40 -0700

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -745,8 +745,8 @@ def prune_while_training(model: nn.Module, arch: str, prune_mode: str, num_class
         
     inplace_precs = []
     #inplace_precs += [test(get_pruned_model(model,[3]))]
-    inplace_precs += [test(prune_by_thresh(model,left=4))]
-    inplace_precs += [test(prune_by_thresh(model,right=4))]
+    inplace_precs += [test(prune_by_thresh(model,left=3.75))]
+    inplace_precs += [test(prune_by_thresh(model,right=3.75))]
     
     print_str = ''
     for flop,prec1,thresh in zip(saved_flops,saved_prec1s,saved_thresh):
