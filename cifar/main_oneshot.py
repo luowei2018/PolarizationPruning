74e542665cf698b9319562fa8365d1e2931613a6
Bo Chen <bochen1993cs@gmail.com>
2022-08-15 18:39:29 -0700

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -632,7 +632,7 @@ def log_quantization(model):
         if order == 1:
             lmask = bin_indices == 0
             rmask = bin_indices == 3
-            x[lmask] -= args.lbd * args.current_lr * 10
+            x[lmask] -= args.lbd * args.current_lr * 1
             x[rmask] -= args.lbd * args.current_lr * 0.1
         else:
             grad = -2 * x + 2 * x_split + args.t
@@ -680,7 +680,7 @@ def log_quantization(model):
     for bn_module in bn_modules:
         with torch.no_grad():
             ch_len = len(bn_module.weight.data)
-            #get_bin_distribution(bn_module.weight.data, assigned_binindices[ch_start:ch_start+ch_len])
+            get_bin_distribution(bn_module.weight.data, assigned_binindices[ch_start:ch_start+ch_len])
             args.bias_err += torch.abs(bn_module.bias.data).sum()
             if args.log_scale:
                 bn_module.weight.data = log_sparsity(bn_module.weight.data, assigned_binindices[ch_start:ch_start+ch_len])
