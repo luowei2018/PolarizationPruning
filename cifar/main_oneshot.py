7e2bc878ddca5dadb3d606b5ec6f735a8387f8b0
Bo Chen <bochen1993cs@gmail.com>
2022-09-01 12:04:34 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -704,6 +704,7 @@ def prune_while_training(model: nn.Module, arch: str, prune_mode: str, num_class
 
     baseline_flops = compute_conv_flops(model, cuda=True)
         
+    print(args.mask_list)
     inplace_precs = []
     for i in range(min(3,len(args.mask_list))):
         inplace_precs += [test(prune_by_mask(model,args.mask_list[:i+1]))]
