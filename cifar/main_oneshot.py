42cfb0158271554f04f1a0ed74d486606aa089c9
Bo Chen <bochen1993cs@gmail.com>
2022-09-01 13:36:21 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -579,7 +579,7 @@ def prune_by_mask(model,mask_list):
             tokeep += freeze_mask.clone().detach()
         
     ch_start = 0
-    print('------------PRUNE-----------')
+    print('------------PRUNE-----------',len(mask_list))
     for bn_module in bn_modules:
         with torch.no_grad():
             ch_len = len(bn_module.weight.data)
@@ -633,15 +633,16 @@ def compare_models(old,new):
     bns1,convs1 = old.get_sparse_layers_and_convs()
     bns2,convs2 = new.get_sparse_layers_and_convs()
     ch_start = 0
-    print('comp:',len(args.mask_list[:args.current_stage]))
     for conv1,bn1,conv2,bn2 in zip(convs1,bns1,convs2,bns2):
         ch_len = conv1.weight.grad.data.size(0)
         for freeze_mask in args.mask_list[:args.current_stage]:
             if freeze_mask is None:continue
             freeze_mask = freeze_mask[ch_start:ch_start+ch_len] == 1
-            assert torch.equal(conv1.weight.data[freeze_mask, :, :, :], conv2.weight.data[freeze_mask, :, :, :])
-            assert torch.equal(bn1.weight.data[freeze_mask], bn2.weight.data[freeze_mask])
-            assert torch.equal(bn1.bias.data[freeze_mask], bn2.bias.data[freeze_mask])
+            #assert torch.equal(conv1.weight.data[freeze_mask, :, :, :], conv2.weight.data[freeze_mask, :, :, :])
+            #assert torch.equal(bn1.weight.data[freeze_mask], bn2.weight.data[freeze_mask])
+            #assert torch.equal(bn1.bias.data[freeze_mask], bn2.bias.data[freeze_mask])
+            print('comp1:',bn1.weight.data[freeze_mask])
+            print('comp2:',bn2.weight.data[freeze_mask])
     
 def factor_visualization(iter, model, prec):
     scale_factors = torch.tensor([]).cuda()
