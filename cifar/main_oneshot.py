3e20fd1ddd4f2ccafa675000568e53e999819104
Bo Chen <bochen1993cs@gmail.com>
2022-09-01 11:33:55 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -754,6 +754,7 @@ def train(epoch):
             avg_sparsity_loss += sparsity_loss.data.item()
         print_model(model)
         loss.backward()
+        print_model(model)
         if args.loss in {LossType.L1_SPARSITY_REGULARIZATION}:
             updateBN()
         if args.loss in {LossType.LOG_QUANTIZATION}:
