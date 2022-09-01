3b1c728aa6c3d8a21f78e15bb968e8ecef69c406
Bo Chen <bochen1993cs@gmail.com>
2022-09-01 11:17:46 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -600,8 +600,8 @@ def log_quantization(model):
             if freeze_mask is None:continue
             with torch.no_grad():
                 #freeze_mask = freeze_mask[ch_start:ch_start+ch_len] == 1
-                print(bn.weight.data.size(),freeze_mask.tolist())
                 bn.weight.grad.data[freeze_mask] = 0
+                print(bn.weight.grad.data.tolist())
                 if hasattr(bn, 'bias') and bn.bias is not None:
                     bn.bias.grad.data[freeze_mask] = 0
                 if isinstance(conv, nn.Conv2d):
