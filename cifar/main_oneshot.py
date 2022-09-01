df3da24f24be893cb5dadb5a8f9601f1063b4ab6
Bo Chen <bochen1993cs@gmail.com>
2022-09-01 15:08:52 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -608,6 +608,9 @@ def freeze_weights(model,old_model):
                 if hasattr(conv1, 'bias') and conv1.bias is not None:
                     conv1.bias.data[freeze_mask] = conv2.bias.data[freeze_mask].clone().detach()
         ch_start += ch_len
+        
+    for name1, param1, name2, param2 in zip(*old.named_parameters(),*new.named_parameters()):
+        param2.data = param1.data.clone().detach()
     
     model.conv1.weight.data = old_model.conv1.weight.data.clone().detach()
     model.bn1.weight.data = old_model.bn1.weight.data.clone().detach()
