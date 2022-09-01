c9ed46d3c96f27b7bad9a8c5e02d75b20c269dd8
Bo Chen <bochen1993cs@gmail.com>
2022-09-01 14:39:17 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -594,10 +594,11 @@ def freeze_weights(model,old_model):
     ch_start = 0
     for conv1,bn1,conv2,bn2 in zip(convs1,bns1,convs2,bns2):
         ch_len = conv1.weight.data.size(0)
-        for freeze_mask in args.mask_list[:args.current_stage]:
+        test_list = [torch.ones(ch_len).long().cuda()]
+        for freeze_mask in test_list:#args.mask_list[:args.current_stage]:
             if freeze_mask is None:continue
             with torch.no_grad():
-                freeze_mask = freeze_mask[ch_start:ch_start+ch_len] == 1
+                freeze_mask = freeze_mask==1#[ch_start:ch_start+ch_len] == 1
                 bn1.weight.data[freeze_mask] = bn2.weight.data[freeze_mask].clone().detach()
                 if hasattr(bn1, 'bias') and bn1.bias is not None:
                     bn1.bias.data[freeze_mask] = bn2.bias.data[freeze_mask].clone().detach()
@@ -763,7 +764,6 @@ def train(epoch):
         if args.loss in {LossType.LOG_QUANTIZATION}:
             freeze_weights(model,old_model)
         compare_models(model,old_model)
-        model = copy.deepcopy(old_model)
         if args.loss in {LossType.POLARIZATION,
                          LossType.L2_POLARIZATION,
                          LossType.LOG_QUANTIZATION}:
