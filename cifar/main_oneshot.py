b4e361750e00bba3fdd9009fd9ef3ec49d030068
Bo Chen <bochen1993cs@gmail.com>
2022-09-05 19:17:22 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -840,14 +840,14 @@ def train(epoch):
             log_quantization(model)
         if args.loss in {LossType.PROGRESSIVE_SHRINKING}:
             print(net_id,freeze_mask.sum())
-            #scale_lr(optimizer,net_id,default_factor=1,reset=False)
+            scale_lr(optimizer,net_id,default_factor=0,reset=False)
         optimizer.step()
         if args.loss in {LossType.LOG_QUANTIZATION}:
             recover_weights(model,old_model,args.mask_list[:args.current_stage])
         if args.loss in {LossType.PROGRESSIVE_SHRINKING}:
             recover_weights(model,old_model,[freeze_mask],keep_extra=(net_id!=3))
-            #scale_lr(optimizer,net_id,reset=True)
-            if net_id!=3:compare_models(old_model,model,[freeze_mask])
+            scale_lr(optimizer,net_id,reset=True)
+            if net_id!=3:compare_models(old_model,model,[freeze_mask],whole=True)
         if args.loss in {LossType.POLARIZATION,
                          LossType.L2_POLARIZATION,
                          LossType.LOG_QUANTIZATION,
