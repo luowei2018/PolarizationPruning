05730c86f390b80189797c8943e826f7a8649141
Bo Chen <bochen1993cs@gmail.com>
2022-09-07 18:39:49 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -639,6 +639,7 @@ def accumulate_grad(old_model,new_model,mask,net_id):
             print(bn1.weight.grad)
             print(bn1.weight)
             optimizer.param_groups[0]['momentum'] = 0
+            print(optimizer)
             optimizer.step()
             print(bn1.weight)
             exit(0)
