0056c1eec481cb0b5492a6adc590e3e09e0ca376
Bo Chen <bochen1993cs@gmail.com>
2022-09-07 23:39:37 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -627,8 +627,9 @@ def accumulate_grad(old_model,new_model,mask,net_id,ch_indices):
                 adjusted_mean[m[start:end]==1] *= 1./(4-i)
                 adjusted_var[m[start:end]==1] *= 1./(4-i)
         else:
-            adjusted_mean *= 1./4
-            adjusted_var *= 1./4
+            #adjusted_mean *= 1./4
+            #adjusted_var *= 1./4
+            pass
         if net_id == 3:
             old_bn.mean_tmp = adjusted_mean
             old_bn.var_tmp = adjusted_var
@@ -879,7 +880,7 @@ def train(epoch):
             optimizer.param_groups[0]['momentum'] = 0
             optimizer.param_groups[1]['momentum'] = 0
             optimizer.param_groups[1]['weight_decay'] = 0
-            freeze_mask,net_id,dynamic_model,ch_indices = sample_network(model,net_id=batch_idx%4)
+            freeze_mask,net_id,dynamic_model,ch_indices = sample_network(model,net_id=3)
         if args.cuda:
             data, target = data.cuda(), target.cuda()
         optimizer.zero_grad()
@@ -918,8 +919,8 @@ def train(epoch):
         if args.loss in {LossType.PROGRESSIVE_SHRINKING}:
             #scale_lr(optimizer,net_id,reset=False)
             accumulate_grad(model,dynamic_model,freeze_mask,net_id,ch_indices)
-        if args.loss not in {LossType.PROGRESSIVE_SHRINKING} or batch_idx%4==3:
-            optimizer.step()
+        #if args.loss not in {LossType.PROGRESSIVE_SHRINKING} or batch_idx%4==3:
+        optimizer.step()
         #if args.loss in {LossType.PROGRESSIVE_SHRINKING}:
         #    fix_weights(model,old_model,[freeze_mask])
         #    scale_lr(optimizer,net_id,reset=True)
