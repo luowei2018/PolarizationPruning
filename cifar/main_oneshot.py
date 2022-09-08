83ff407fe40eda85448ce3d45a61587ba65ec0a4
Bo Chen <bochen1993cs@gmail.com>
2022-09-08 00:19:22 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -603,7 +603,7 @@ def prune_by_mask(old_model,mask_list,zero_bias=True):
     return pruned_model
     
 
-args.training_factor= [1,0.01,0.001,0.0001]
+args.training_factor= [1,1,1,1]#[1,0.01,0.001,0.0001]
     
 def accumulate_grad(old_model,new_model,mask,net_id,ch_indices):
     def helper(old_param,new_param):
