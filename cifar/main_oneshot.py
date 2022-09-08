80eed139f268ebbb99f45f5cc8e71282928c5d68
Bo Chen <bochen1993cs@gmail.com>
2022-09-07 19:40:42 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -688,16 +688,12 @@ def compare_models(old,new,mask_list,whole=False):
     total_changed = torch.tensor([]).cuda()
     for conv1,bn1,conv2,bn2 in zip(convs1,bns1,convs2,bns2):
         if not whole:
-            if ch_start == 0:
-                print('1::::::::::::')
-                print(bn1.weight)
-                print(bn1.weight.grad)
-                print('2::::::::::::::::')
-                print(bn2.weight)
             ch_len = conv1.weight.data.size(0)
             for freeze_mask in mask_list:
                 freeze_mask = freeze_mask[ch_start:ch_start+ch_len] == 1
-                assert torch.equal(bn1.weight.data[freeze_mask], bn2.weight.data[freeze_mask])
+                print(bn1.weight.data[freeze_mask])
+                print(bn2.weight.data[freeze_mask])
+                assert torch.equal(bn1.weight.data[freeze_mask], bn2.weight.data[freeze_mask]) 
                 assert torch.equal(conv1.weight.data[freeze_mask, :, :, :], conv2.weight.data[freeze_mask, :, :, :])
                 assert torch.equal(bn1.bias.data[freeze_mask], bn2.bias.data[freeze_mask])
                 assert torch.equal(bn1.running_mean.data[freeze_mask],bn2.running_mean.data[freeze_mask])
