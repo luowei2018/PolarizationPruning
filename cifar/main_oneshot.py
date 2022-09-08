e2fec9f37913a569bcca47c2832041e7c403428d
Bo Chen <bochen1993cs@gmail.com>
2022-09-07 19:54:04 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -889,12 +889,10 @@ def train(epoch):
             #scale_lr(optimizer,net_id,reset=False)
             accumulate_grad(model,dynamic_model,freeze_mask,net_id)
         #if args.loss not in {LossType.PROGRESSIVE_SHRINKING} or batch_idx%4==3:
-        compare_models(model,old_model,[freeze_mask],whole=False)
         optimizer.step()
         #if args.loss in {LossType.PROGRESSIVE_SHRINKING}:
         #    fix_weights(model,old_model,[freeze_mask])
         #    scale_lr(optimizer,net_id,reset=True)
-        compare_models(model,old_model,[freeze_mask],whole=False)
         if args.loss in {LossType.POLARIZATION,
                          LossType.L2_POLARIZATION}:
             clamp_bn(model, upper_bound=args.clamp)
