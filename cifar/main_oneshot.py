b08b0144221805c87991db834f3a566525b11c3c
Bo Chen <bochen1993cs@gmail.com>
2022-09-15 22:11:53 -0500

    Update main_oneshot.py

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -499,13 +499,12 @@ def sample_network(old_model,net_id=None,eval=False):
                 # updated in the last update
                 bn_module.running_mean.data = bn_module._buffers[f"mean{net_id}"]
                 bn_module.running_var.data = bn_module._buffers[f"var{net_id}"]
-    for bn_module in old_model.get_sparse_layers():
+    for bn_module in old_model.modules():
         assert hasattr(bn_module,'mean0')
     dynamic_model = copy.deepcopy(old_model)
     bn_modules = dynamic_model.get_sparse_layers()
     for bn_module in bn_modules:
         all_scale_factors = torch.cat((all_scale_factors,bn_module.weight.data))
-        assert hasattr(bn_module,'mean0')
     
     # total channels
     total_channels = len(all_scale_factors)
