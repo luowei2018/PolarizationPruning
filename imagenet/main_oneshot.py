32c76376e208865976595bcef73db32dfe74595f
Bo Chen <bochen1993cs@gmail.com>
2022-09-16 09:22:15 -0500

    Update main_oneshot.py

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -1057,6 +1057,7 @@ def sample_network(args,old_model,net_id=None,eval=False):
     _,ch_indices = all_scale_factors.sort(dim=0)
     
     weight_valid_mask = torch.zeros(total_channels).long().cuda()
+    print(ch_indices[channel_per_layer*(num_subnets-1-net_id):])
     weight_valid_mask[ch_indices[channel_per_layer*(num_subnets-1-net_id):]] = 1
         
     freeze_mask = 1-weight_valid_mask
