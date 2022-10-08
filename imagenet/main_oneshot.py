df2e230fef42735b726b42f8ed839f1eb57b764a
Bo Chen <bochen1993cs@gmai.com>
2022-10-08 10:52:22 -0500

    test

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -661,8 +661,10 @@ def main_worker(gpu, ngpus_per_node, args):
 
     print("rank #{}: dataloader loaded!".format(args.rank))
     if args.evaluate:
+        validate(train_loader, model, criterion, epoch=0, args=args, writer=None)
+        exit(0)
         for fake_prune in [True,False]:
-            prec1,prune_str,saved_prec1s = prune_while_training(model, args.arch, args.prune_mode, args.width_multiplier, train_loader, criterion, 0, args, fake_prune=fake_prune, check_size=fake_prune)
+            prec1,prune_str,saved_prec1s = prune_while_training(model, args.arch, args.prune_mode, args.width_multiplier, val_loader, criterion, 0, args, fake_prune=fake_prune, check_size=fake_prune)
             print(args.save,prune_str,args.alphas)
         return
 
