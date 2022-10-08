80075c5df5e65ec8c426d0d151933ee495d96f6e
Bo Chen <bochen1993cs@gmai.com>
2022-10-08 12:41:24 -0500

    test

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -1085,7 +1085,6 @@ def sample_network(args,old_model,net_id=None,eval=False,fake_prune=True,check_s
                 bn_module.mean_sum = []
                 bn_module.var_sum = []
                 bn_module.sum_len = 0
-                bn_module.eval()
     
     if isinstance(dynamic_model, nn.DataParallel) or isinstance(dynamic_model, nn.parallel.DistributedDataParallel):
         bn_modules,convs = dynamic_model.module.get_sparse_layers_and_convs()
@@ -1393,7 +1392,9 @@ def train(train_loader, model, criterion, optimizer, epoch, sparsity, args, is_d
                 freeze_mask,net_id,dynamic_model,ch_indices = sample_network(args,model,net_id)
             else:
                 freeze_mask,net_id,dynamic_model,ch_indices = sample_network(args,model)
-        print(dynamic_model.module.get_sparse_layers_and_convs()[0][0].running_mean)
+
+        assert torch.equal(dynamic_model.module.get_sparse_layers_and_convs()[0][0].running_mean,model.module.get_sparse_layers_and_convs()[0][0].running_mean)
+        assert torch.equal(dynamic_model.module.get_sparse_layers_and_convs()[0][0].running_var,model.module.get_sparse_layers_and_convs()[0][0].running_var)
         # the adjusting only work when epoch is at decay_epoch
         adjust_learning_rate(optimizer, epoch, lr=args.lr, decay_epoch=args.decay_epoch,
                              total_epoch=args.epochs,
