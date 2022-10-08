9cd12142247d0333fb34d112f7cb06046b4790df
Bo Chen <bochen1993cs@gmai.com>
2022-10-08 14:41:34 -0500

    correct minibatch bias

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -1084,8 +1084,8 @@ def sample_network(args,old_model,net_id=None,eval=False,fake_prune=True,check_s
                 if num_mini_batch != 1:
                     bn_module.eval()
                     def bn_fordward_hook(self, input, output):
-                        self.minibatch_mean = input.mean([0, 2, 3])
-                        self.minibatch_var = input.var([0, 2, 3], unbiased=False)
+                        self.minibatch_mean = input[0].mean([0, 2, 3])
+                        self.minibatch_var = input[0].var([0, 2, 3], unbiased=False)
                     bn_module.register_forward_hook(bn_fordward_hook)
 
     
