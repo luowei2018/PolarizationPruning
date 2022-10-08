e336270fd4467e8dd4c0f02b0e99dc196df673c2
Bo Chen <bochen1993cs@gmai.com>
2022-10-07 22:43:02 -0500

    val

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -1225,8 +1225,8 @@ def update_shared_model(args,old_model,new_model,mask,batch_idx,ch_indices,net_i
                 assert len(old_module.masks)==4
                 for i in range(1,4):
                     assert old_module.masks[i].sum()>=old_module.masks[i-1].sum() 
-                    assert old_module.masks[i][old_module.masks[i-1]==1].min()==old_module.masks[i][old_module.masks[i-1]==1].max() 
-                    assert old_module.masks[i][old_module.masks[i-1]==1].min()==1
+                    assert min(old_module.masks[i][old_module.masks[i-1]==1])==max(old_module.masks[i][old_module.masks[i-1]==1])
+                    assert min(old_module.masks[i][old_module.masks[i-1]==1])==1
                 assert old_module.masks[-1].sum()==len(old_module.masks[-1])
                 old_module.masks=[]
 
