1fed485d077a1201726a0b89a9521a0460c5cc5a
Bo Chen <bochen1993cs@gmai.com>
2022-10-14 23:28:20 -0500

    _

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -686,7 +686,7 @@ def main_worker(gpu, ngpus_per_node, args):
               is_debug=args.debug)
 
         # prune the network and record FLOPs at each epoch
-        if epoch >= 30:
+        if epoch >= 90:
             prec1,prune_str,saved_prec1s = prune_while_training(model, args.arch, args.prune_mode, args.width_multiplier, val_loader, criterion, epoch, args, avg_loss=avg_loss, fake_prune=True)
             print(f"Epoch {epoch}/{args.epochs}",args.arch,args.save,prune_str,args.alphas)
         else:
