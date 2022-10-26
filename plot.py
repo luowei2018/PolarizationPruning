71f9313ac2c064b69fc7b6326dd2e5d0d003b2af
Bo Chen <bochen1993cs@gmai.com>
2022-10-26 18:53:50 -0500

    _

--- plot.py
+++ plot.py
@@ -10,10 +10,19 @@ linewidth = 2
 plt.rcParams['xtick.labelsize'] = 16
 plt.rcParams['ytick.labelsize'] = 16
 plt.rcParams["font.family"] = "Times New Roman"
-colors = ['#DB1F48','#FF9636','#1C4670','#9D5FFB','#21B6A8','#D65780']
-# colors = ['#D00C0E','#E09C1A','#08A720','#86A8E7','#9D5FFB','#D65780']
+# colors = ['#DB1F48','#FF9636','#1C4670','#9D5FFB','#21B6A8','#D65780']
+colors = ['#d53e4f',
+'#f46d43',
+'#fdae61',
+'#fee08b',
+'#e6f598',
+'#abdda4',
+'#66c2a5',
+'#3288bd'
+]
 labels = ['LSVC','H.264','H.265','DVC','RLVC']
-markers = ['o','P','s','>','D','^']
+# markers = ['o','P','s','>','D','^']
+markers = ['o','P','s','D','v','^','<','>']
 linestyles = ['solid','dotted','dashed','dashdot', (0, (3, 5, 1, 5, 1, 5))]
 
 
@@ -32,7 +41,7 @@ def line_plot(XX,YY,label,color,path,xlabel,ylabel,lbsize=labelsize_b,lfsize=lab
 			plt.plot(xx, yy, color = color[i], marker = markers[i], 
 				# linestyle = linestyles[i], 
 				label = label[i], 
-				linewidth=1, markersize=4)
+				linewidth=2, markersize=6)
 		else:
 			plt.errorbar(xx, yy, yerr=yerr[i], color = color[i], 
 				marker = markers[i], label = label[i], 
@@ -51,22 +60,22 @@ def line_plot(XX,YY,label,color,path,xlabel,ylabel,lbsize=labelsize_b,lfsize=lab
 	# 		plt.legend(loc=legloc,fontsize = lfsize,ncol=ncol)
 
 	plt.legend(bbox_to_anchor=(0.46, 1.28), fancybox=True,
-	           loc='upper center', ncol=5, fontsize=lfsize)
+	           loc='upper center', ncol=4, fontsize=lfsize)
 
 	plt.xlim((0,100))
 	# plt.ylim((0,95))
 
 	# inset axes....
-	axins = ax.inset_axes([0.02, 0.08, 0.30, 0.65])
+	axins = ax.inset_axes([0.02, 0.04, 0.30, 0.65])
 	for i in range(len(XX)):
-		xx,yy = XX[i][5:],YY[i][5:]
+		xx,yy = XX[i],YY[i]
 		xx = np.array(xx)
 		yy = np.array(yy)*100
 		axins.plot(xx, yy, color = color[i], marker = markers[i], 
 			label = label[i], 
-			linewidth=1, markersize=4)
+			linewidth=2, markersize=6)
 	# sub region of the original image
-	x1, x2, y1, y2 = 0, 60, 88, 96
+	x1, x2, y1, y2 = 0, 60, 90, 95
 	axins.set_xlim(x1, x2)
 	axins.set_ylim(y1, y2)
 	axins.set_xticklabels([])
@@ -90,10 +99,10 @@ def line_plot(XX,YY,label,color,path,xlabel,ylabel,lbsize=labelsize_b,lfsize=lab
 
 	ax.indicate_inset_zoom(axins, edgecolor="gray")
 
-	ratio = 0.3
-	xleft, xright = ax.get_xlim()
-	ybottom, ytop = ax.get_ylim()
-	ax.set_aspect(abs((xright-xleft)/(ybottom-ytop))*ratio)
+	# ratio = 1
+	# xleft, xright = ax.get_xlim()
+	# ybottom, ytop = ax.get_ylim()
+	# ax.set_aspect(abs((xright-xleft)/(ybottom-ytop))*ratio)
 
 
 	plt.tight_layout()
@@ -140,9 +149,6 @@ def line_plot(XX,YY,label,color,path,xlabel,ylabel,lbsize=labelsize_b,lfsize=lab
 
 x = []
 y = []
-# onion
-y += [[0.1067,0.5651,0.8049,0.8701,0.8972,0.9159,0.9288,0.9330,0.9382,0.9399,0.9423,0.9429]]
-x += [[92.67,90.62,86.42,80.16,72.02,64.39,55.75,46.84,36.59,27.71,18.18,0.00]]
 # original
 x += [[92.52,87.53,80.16,71.11,61.96,51.42,40.52,30.26,20.15,0.00]]
 y += [[0.1040,0.1216,0.1160,0.1655,0.1820,0.2294,0.4102,0.8530,0.9135,0.9411]]
@@ -155,6 +161,18 @@ y += [[0.1000,0.0927,0.1090,0.2734,0.7647,0.8648,0.9242,0.9310,0.9310,0.9322]]
 # ofa
 x += [[90.70,86.88,80.53,72.31,64.04,57.53,50.84,40.20,22.81,0.00]]
 y += [[0.3587,0.6448,0.8675,0.9009,0.9103,0.9081,0.9082,0.9084,0.9086,0.9086]]
-line_plot(x, y,['Ours','Original','Lasso','Polar','OFANet'],colors,
+# onion 4
+y += [[0.8254,0.9216,0.9398,0.9425]]
+x += [[84.77,62.79,36.12,0.00]]
+# ONN8
+y += [[0.4266,0.8285,0.8929,0.9195,0.9300,0.9370,0.9402,0.9427]]
+x += [[93.22,86.11,76.02,64.22,51.33,36.68,23.35,0.00]]
+# ONN12
+y += [[0.1067,0.5651,0.8049,0.8701,0.8972,0.9159,0.9288,0.9330,0.9382,0.9399,0.9423,0.9429]]
+x += [[92.67,90.62,86.42,80.16,72.02,64.39,55.75,46.84,36.59,27.71,18.18,0.00]]
+# ONN16
+y += [[0.1320,0.1590,0.5718,0.8065,0.8238,0.8444,0.8881,0.9162,0.9228,0.9283,0.9355,0.9357,0.9375,0.9391,0.9427,0.9433]]
+x += [[92.95,92.78,88.97,86.40,81.69,76.06,70.21,64.26,58.40,51.57,44.47,36.67,30.07,23.33,14.82,0.00]]
+line_plot(x, y,['Original','Lasso','Polar','OFA','ONN-L4','ONN-L8','ONN-L12','ONN-L16'],colors,
 		'/home/bo/Dropbox/Research/CVPR23/images/compare_scalability.eps',
-		'Pruned FLOPS (%)','Top1 Accuracy (%)',yticks=[50,90],xticks=[i*10 for i in range(1,10)],use_arrow=True)	
+		'Pruned FLOPS (%)','Top1 Accuracy (%)',yticks=[i*10 for i in range(10)],xticks=[i*10 for i in range(1,10)],use_arrow=True)	
