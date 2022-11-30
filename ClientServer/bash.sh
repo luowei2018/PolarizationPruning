7ee58a06ee56c3bdc036d9767f37743bb7f0ed3b
luowei2018 <42980027+luowei2018@users.noreply.github.com>
2022-11-30 14:52:15 -0500

    Update bash.sh

--- ClientServer/bash.sh
+++ ClientServer/bash.sh
@@ -5,7 +5,7 @@
 # python3 client.py --host 155.98.38.150 --port 9007 --size 1408 --time 10000
 # python3 client.py --host 155.98.38.150 --port 9007 --size 2816 --time 10000
 # python3 client.py --host 155.98.38.150 --port 9007 --size 5632 --time 10000
-python3 client.py --host 155.98.38.150 --port 9007 --size 12 --time 2881
+python3 client.py --host 155.98.38.150 --port 9007 --size 12 --time 10000
 python3 client.py --host 155.98.38.150 --port 9007 --size 24 --time 10000
 python3 client.py --host 155.98.38.150 --port 9007 --size 48 --time 10000
 python3 client.py --host 155.98.38.150 --port 9007 --size 96 --time 10000
