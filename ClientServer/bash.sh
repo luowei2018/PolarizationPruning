46f7fd4bf7ee282d5e36cedb7c0ba081b0046558
luowei2018 <42980027+luowei2018@users.noreply.github.com>
2022-12-01 18:57:31 -0500

    Update bash.sh

--- ClientServer/bash.sh
+++ ClientServer/bash.sh
@@ -1,3 +1,5 @@
+python3 client.py --host 155.98.38.150 --port 9007 --size 22 --time 10000
+python3 client.py --host 155.98.38.150 --port 9007 --size 44 --time 10000
 # python3 client.py --host 155.98.38.150 --port 9007 --size 88 --time 10000
 # python3 client.py --host 155.98.38.150 --port 9007 --size 176 --time 10000
 # python3 client.py --host 155.98.38.150 --port 9007 --size 352 --time 10000
@@ -5,10 +7,10 @@
 # python3 client.py --host 155.98.38.150 --port 9007 --size 1408 --time 10000
 # python3 client.py --host 155.98.38.150 --port 9007 --size 2816 --time 10000
 # python3 client.py --host 155.98.38.150 --port 9007 --size 5632 --time 10000
-python3 client.py --host 155.98.38.150 --port 9007 --size 12 --time 10000
-python3 client.py --host 155.98.38.150 --port 9007 --size 24 --time 10000
-python3 client.py --host 155.98.38.150 --port 9007 --size 48 --time 10000
-python3 client.py --host 155.98.38.150 --port 9007 --size 96 --time 10000
-python3 client.py --host 155.98.38.150 --port 9007 --size 192 --time 10000
-python3 client.py --host 155.98.38.150 --port 9007 --size 384 --time 10000
-python3 client.py --host 155.98.38.150 --port 9007 --size 768 --time 10000
+# python3 client.py --host 155.98.38.150 --port 9007 --size 12 --time 10000
+# python3 client.py --host 155.98.38.150 --port 9007 --size 24 --time 10000
+# python3 client.py --host 155.98.38.150 --port 9007 --size 48 --time 10000
+# python3 client.py --host 155.98.38.150 --port 9007 --size 96 --time 10000
+# python3 client.py --host 155.98.38.150 --port 9007 --size 192 --time 10000
+# python3 client.py --host 155.98.38.150 --port 9007 --size 384 --time 10000
+# python3 client.py --host 155.98.38.150 --port 9007 --size 768 --time 10000
